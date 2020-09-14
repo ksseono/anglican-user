4b977f7bbde8fe1186ab909cd6fedb505eed8473
Seonho Kim <ksseono@gmail.com>
2020-09-14 23:13:40 +0900

    update clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [nstools "0.2.4"]
                  [anglican "1.0.0"]]
   :plugins [[org.clojars.benfb/lein-gorilla "0.7.0"]]
